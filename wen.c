de36adf38c43453eb27aeeb7472bf1749a20a955
chakri <86776511+chakrinya009@users.noreply.github.com>
2021-11-01 00:20:05 +0530

    Update wen.c

--- wen.c
+++ wen.c
@@ -5,6 +5,7 @@ void main()
  int n;
   scanf("%d",&n);
   BALABHADRA CHAKRINYA SRI RAMA SAI ESWAR
+   
   balabhadra chakrinya sri rama sai eswar
    Balabhadra chakrinya sri rama sai eswar
   getch();
